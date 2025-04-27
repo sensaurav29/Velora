a116a2a8b6c9c0536691ef02ca2ca9f6d76bc41a
unknown <sensaurav25@gmail.com>
2025-04-27 22:48:30 +0530

    routes and checks added for dev only access

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
+const config = require("config");
 
-mongoose.connect("mongodb://localhost:27017/veloraapp")
+const dbgr = require("debug")("development:mongoose");
+
+
+mongoose.connect(`${config.get("MONGODB_URI")}/veloraapp`)
 .then(function(){
-    console.log('connected');
+    dbgr("connected");
 })
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
 })
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,29 @@
 const expess = require("express");
 const router = expess.Router();
+const ownerModel = require("../models/owner-model");
 
-router.get("/", (req ,res ) => {
-    res.send("hey it's working");
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res
+        .status(503)
+        .send("You don't have the permission to create a new owner.");
+    }
+
+    let {fullname, email, password} = req.body;
+
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+
+    res.status(201).send(createdOwner);
+  });
+}
+router.get("/", (req, res) => {
+  res.send("hey it's working");
 });
 
 module.exports = router;
